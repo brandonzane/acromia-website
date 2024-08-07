3ffeb6d1c024cb410c2f5d6d6bfdfd825ac2a559
brandonzane <bmushori@gmail.com>
2024-08-07 12:54:41 +0200

    Initial commit

---
+++ src/sections/Header.tsx
@@ -0,0 +1,35 @@
+import ArrowRight from "../assets/arrow-right.svg";
+import Logo from "../assets/logosaas.png";
+import Image from "next/image";
+import MenuIcon from "../assets/menu.svg";
+
+export const Header = () => {
+  return (
+    <header className="sticky top-0">
+      <div className="flex justify-center items-center py-3 bg-black text-white text-sm">
+        <div className="inlie-flex gap-1 items-center">
+          <p>Get Started for Free</p>
+          <ArrowRight className="w-4 h-4 inline-flex justify-center items-center" />
+        </div>
+      </div>
+      <div className="py-5">
+        <div className="container">
+          <div className="flex items-center justify-between">
+            <Image src={Logo} alt="logo" height={40} width={40} />
+            <MenuIcon className="w-5 h-5 md:hidden" />
+            <nav className="hidden md:flex gap-6 text-black/60 items-center">
+              <a href="#">About</a>
+              <a href="#">Features</a>
+              <a href="#">Customers</a>
+              <a href="#">Updates</a>
+              <a href="#">Help</a>
+              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight">
+                Get for it free
+              </button>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
