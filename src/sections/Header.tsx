fdf04fa4f6bfd3e1b618b925a87435b2da189bc9
brandonzane <bmushori@gmail.com>
2024-08-28 16:09:28 +0200

    Added functionality for the menu

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,29 +4,45 @@ import ArrowRight from "@/assets/arrow-right.svg";
 import acromiaLogo from "@/assets/acromia-logo.png";
 import Image from "next/image";
 import MenuIcon from "@/assets/menu.svg";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export const Header = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   const scrollToSection = useCallback((elementId: string) => {
     const element = document.getElementById(elementId);
     if (element) {
       element.scrollIntoView({ behavior: "smooth" });
     }
+    setMobileMenuOpen(false);
   }, []);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
+  const closeMobileMenu = useCallback(
+    (event: MouseEvent) => {
+      if (mobileMenuOpen) {
+        const header = document.querySelector("header");
+        if (header && !header.contains(event.target as Node)) {
+          setMobileMenuOpen(false);
+        }
+      }
+    },
+    [mobileMenuOpen]
+  );
+
+  useEffect(() => {
+    document.addEventListener("click", closeMobileMenu);
+    return () => {
+      document.removeEventListener("click", closeMobileMenu);
+    };
+  }, [closeMobileMenu]);
 
   return (
     <header className="sticky top-0 z-20 backdrop-blur-sm">
-      <div className="flex items-center justify-center gap-3 bg-black py-3 text-sm text-white">
-        <p className="hidden text-white/60 md:block">
-          Streamline your marketing workflow
-        </p>
-        <div className="inline-flex items-center gap-1">
-          <p>Get started with the Professional Plan</p>
-          <ArrowRight className="inline-flex size-4 items-center justify-center" />
-        </div>
-      </div>
-
-      <div className="py-5">
+      <div className="py-5 relative">
         <div className="container">
           <div className="flex items-center justify-between">
             <Image
@@ -35,23 +51,49 @@ export const Header = () => {
               width={160}
               alt="Light SaaS Landing Page Logo"
             />
-            <MenuIcon className="size-5 md:hidden" />
-            <nav className="hidden items-center gap-6 text-black/60 md:flex">
-              <a onClick={() => scrollToSection("customers")} href="#customers">
+            <MenuIcon
+              className="size-5 md:hidden cursor-pointer"
+              onClick={toggleMobileMenu}
+            />
+            <nav
+              className={`
+              ${
+                mobileMenuOpen
+                  ? "max-h-screen opacity-100"
+                  : "max-h-0 opacity-0"
+              }
+              md:max-h-screen md:opacity-100
+              transition-all duration-700 ease-in-out rounded-b-lg
+              overflow-hidden absolute top-full left-0 right-0 md:relative md:top-auto
+              bg-black/80 backdrop-blur-sm md:bg-transparent md:backdrop-blur-none
+              flex flex-col md:flex-row items-center md:items-stretch gap-6 md:gap-6 text-white/80
+            `}
+            >
+              <a
+                onClick={() => scrollToSection("customers")}
+                href="#customers"
+                className="py-2 md:py-0 hover:text-black transition-colors duration-200"
+              >
                 Customers
               </a>
-              <a onClick={() => scrollToSection("products")} href="#products">
+              <a
+                onClick={() => scrollToSection("products")}
+                href="#products"
+                className="py-2 md:py-0 hover:text-black transition-colors duration-200"
+              >
                 Products
               </a>
-              <a onClick={() => scrollToSection("pricing")} href="#pricing">
+              <a
+                onClick={() => scrollToSection("pricing")}
+                href="#pricing"
+                className="py-2 md:py-0 hover:text-black transition-colors duration-200"
+              >
                 Pricing
               </a>
-              {/* <a href="#">Updates</a>
-              <a href="#">Help</a> */}
               <a
                 onClick={() => scrollToSection("booking")}
                 href="#booking"
-                className="btn btn-primary"
+                className="btn btn-primary my-2 md:my-0"
               >
                 Book a call
               </a>
